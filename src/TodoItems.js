488789d3a982653296c0cbfdea9e63a9931b35ca
Roman <romanlabzin03@gmail.com>
2023-05-22 00:36:17 +0500

    о_0

---
+++ src/TodoItems.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class TodoItems extends Component {
+    constructor(props) {
+        super(props);
+
+        this.createTask = this.createTask.bind(this);
+    }
+
+    delete(key) {
+        this.props.delete(key);
+    }
+
+    createTask(item) {
+        return <li onClick={() => this.delete(item.key)}
+        key={item.key}>{item.text}</li>
+    }
+
+    render() {
+        let todoEntries = this.props.entries;
+        let listItems = todoEntries.map(this.createTask)
+
+        return (
+            <ul className="theList">
+                {listItems}
+            </ul>
+        );
+    }
+}
+
+export default TodoItems;
